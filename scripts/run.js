2817c94db56ca28a2289fe64e074402fe5c48cd3
Paul Kiage <paul.r.kiage@gmail.com>
2022-03-06 22:41:59 +0300

    feat: use jpg for faster rendering

--- scripts/run.js
+++ scripts/run.js
@@ -2,15 +2,15 @@ const main = async () => {
   const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
   const gameContract = await gameContractFactory.deploy(
       ["Water", "Earth", "Air","Fire"],       // Names
-      ["QmRrbohrbCBgFhAp397RWpxwToYEPknd7gCDUwBLTF5QDH", // Images
-      "QmXMgM72ni9wdnUaA54dTZD6BdGSjUNNrhkN5eFS8bpAXy", 
-      "QmWyzhxxwcsHYqrySHRepwvyp4R5GKeZeJY7ZeXGWtJTbL",
-      "QmPX3hw1L3UWY8aN5voQmhPTgQ65nvstxin9XuK6eXUHZM"
+      ["https://i.postimg.cc/769FpfD8/water.jpg", // Images https://cloudflare-ipfs.com/ipfs/QmPX3hw1L3UWY8aN5voQmhPTgQ65nvstxin9XuK6eXUHZM
+      "https://i.postimg.cc/HxTGZbq9/earth.jpg", 
+      "https://i.postimg.cc/W1JPhNmg/air.jpg",
+      "https://i.postimg.cc/N0qvX3bV/fire.jpg"
     ],
       [100, 300, 100,100],                    // HP values
       [100, 25, 50,50],                       // Attack damage values
       "Fire", // Boss name
-      "QmSat7rvhFMSztLTLE4UQLZtMBbexNm1vPLDo2QP5UkteH", // Boss image
+      "https://i.postimg.cc/5tBdSsRt/forestfire.jpg", // Boss image
       10000, // Boss hp
       50 // Boss attack damage
   );
