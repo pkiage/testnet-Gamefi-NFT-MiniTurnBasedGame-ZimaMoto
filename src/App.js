93e1de6e9ac5464ab87b6e30976a9511af32f5e5
Paul Kiage <paul.r.kiage@gmail.com>
2022-03-06 23:00:43 +0300

    cleanup: remove unused twitter logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {useEffect, useState} from 'react';
-import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 import SelectCharacter from './Components/SelectCharacter';
 import { CONTRACT_ADDRESS, transformCharacterData } from './constants';
